242d8d1ac1f2654832435150d1bb8b288b3c80c4
rip000000 <wjma829@gmail.com>
2026-09-17 05:01:52 +0300

    Update cron-wednesday.js

--- api/cron-wednesday.js
+++ api/cron-wednesday.js
@@ -1,4 +1,5 @@
-import { kv } from '@vercel/kv';
+import { Redis } from '@upstash/redis'
+const redis = Redis.fromEnv();
 export default async function handler(req, res) {
   const list = await kv.get('patientList') || [];
   const pending = list.filter(p => p.status === "待勾选");
