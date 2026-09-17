475cfe40a0728b72bc7158d713bfb344931f340b
rip000000 <wjma829@gmail.com>
2026-09-17 05:02:20 +0300

    Update list.js

--- api/list.js
+++ api/list.js
@@ -1,4 +1,5 @@
-import { kv } from '@vercel/kv';
+import { Redis } from '@upstash/redis'
+const redis = Redis.fromEnv();
 export default async function handler(req, res) {
   const patientList = await kv.get('patientList') || [];
   const archiveList = await kv.get('archiveList') || [];
