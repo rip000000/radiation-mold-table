7eb833dd2928057e1a00966c76a481c1aa364a79
rip000000 <wjma829@gmail.com>
2026-09-17 06:44:57 +0300

    Update tech-archive.js

--- api/tech-archive.js
+++ api/tech-archive.js
@@ -1,10 +1,11 @@
 import { Redis } from '@upstash/redis'
 const redis = Redis.fromEnv();
+
 export default async function handler(req, res) {
   if(req.method !== 'POST') return res.status(405).end();
   const {pid} = req.body;
-  let patientList = await kv.get('patientList') || [];
-  let archiveList = await kv.get('archiveList') || [];
+  let patientList = await redis.get('patientList') || [];
+  let archiveList = await redis.get('archiveList') || [];
   const idx = patientList.findIndex(p=>p.id===pid);
   if(idx >=0){
     const p = patientList[idx];
@@ -12,7 +13,7 @@ export default async function handler(req, res) {
     archiveList.push(p);
     patientList.splice(idx,1);
   }
-  await kv.set('patientList', patientList);
-  await kv.set('archiveList', archiveList);
+  await redis.set('patientList', patientList);
+  await redis.set('archiveList', archiveList);
   res.status(200).json({ok:true});
 }
