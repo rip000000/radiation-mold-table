4f8af4d97050f3f2d1b0137e6424006b6e3fcc78
rip000000 <wjma829@gmail.com>
2026-09-17 06:44:39 +0300

    Update list.js

--- api/list.js
+++ api/list.js
@@ -1,7 +1,8 @@
 import { Redis } from '@upstash/redis'
 const redis = Redis.fromEnv();
+
 export default async function handler(req, res) {
-  const patientList = await kv.get('patientList') || [];
-  const archiveList = await kv.get('archiveList') || [];
+  const patientList = await redis.get('patientList') || [];
+  const archiveList = await redis.get('archiveList') || [];
   res.status(200).json({ patientList, archiveList });
 }
